ba105aeaed8d2a802ff7fd6388c476e84cc71af6
JhonyAlex <39017484+JhonyAlex@users.noreply.github.com>
2025-04-22 14:17:04 +0000

    organizar en archivos

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBp5a0lUJYZWhtUTVDuwn1jvj6TodaCyrc", // Consider using environment variables for sensitive keys in a real deployment
+    authDomain: "production-control-pigmea.firebaseapp.com",
+    projectId: "production-control-pigmea",
+    storageBucket: "production-control-pigmea.firebasestorage.app",
+    messagingSenderId: "1008458265879",
+    appId: "1:1008458265879:web:008889d56b1d4de3cc3352"
+};
